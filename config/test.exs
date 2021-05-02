9ca52111923272339696b22c78220dfb583c8120
Alex Shakun <alx.safari@gmail.com>
2021-05-02 15:25:11 +0300

    try

--- config/test.exs
+++ config/test.exs
@@ -13,7 +13,7 @@ config :logger, level: :warn
 config :shortener, Shortener.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
+  password: "password",
   database: "shortener_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
