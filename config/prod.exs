966f00ed77a3b592947115998e94e942dc942e7b
Alex Shakun <alx.safari@gmail.com>
2021-10-17 21:51:11 +0300

    ssl

--- config/prod.exs
+++ config/prod.exs
@@ -23,7 +23,8 @@ config :shortener, ShortenerWeb.Endpoint,
 config :shortener, Shortener.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
 
 # Do not print debug messages in production
 config :logger, level: :info
